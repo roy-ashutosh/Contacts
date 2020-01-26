7a22de0c2bfc615df99a7b2415767b761e25d4d6
Ashutosh <royashutosh1991@gmail.com>
2020-01-27 01:31:08 +0530

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Ashutosh Roy on 27/01/20.
+//  Copyright © 2020 Ashutosh Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ContactsTests/ContactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ContactsTests.swift
+//  ContactsTests
+//
+//  Created by Ashutosh Roy on 27/01/20.
+//  Copyright © 2020 Ashutosh Roy. All rights reserved.
+//
+
+import XCTest
+@testable import Contacts
+
+class ContactsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
